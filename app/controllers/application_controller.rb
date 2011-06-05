d1ff5fe9b6dfe0bca97096f70de0c96d90abc543
Chris Rowe <mail@chrisrowe.net>
2011-06-05 20:20:25 +0100

    logic moved to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,33 @@ class ApplicationController < ActionController::Base
   
   def index
     @players = Player.all
+    @players.each do |player|
+      player[:wins] = Game.where(:winner => player.id).count
+      player[:loses] = Game.where(:player_id => player.id).count + Game.where(:opponent_id => player.id).count - Game.where(:winner => player.id).count
+      player[:played] = player[:wins] + player[:loses]
+    
+      ratio = player[:wins]/player[:played].to_f
+      if ratio.nan?
+        ratio = 0.0
+      end
+    
+      player[:ratio] = ratio
+    end
+    
+    @players.sort_by { |player| player.ratio }
   end
+  
+
+  # 
+  # def ratio
+  #   wins = Game.where(:winner => id).count
+  #   played = Game.where(:player_id => id).count + Game.where(:opponent_id => id).count
+  #   ratio = wins/played.to_f
+  #   if !ratio.nan?
+  #     ratio
+  #   else
+  #     0.0
+  #   end
+  # end
   
 end
