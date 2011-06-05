83d7e053307df0a3425e38ddbcd0b879c3b13a88
Chris Rowe <chrisrowe@Chris-Rowes-MacBook-Air.local>
2011-06-05 13:12:13 +0100

    Initial

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Unit16pool::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unit16pool::Application.config.secret_token = '73bc98271bdb972d0e8918cccf485b853bd9ddd15da10168c51b9733797aad50715c75cfb900522f43af28a4cf0f25260a0fcd60a356d0607d3d533cee457e4d'
